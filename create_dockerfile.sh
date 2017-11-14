51a8f064e566ad618d1aecb4ab5b11986a9159b8
cccbauer <clemensbauer.c.c.bauer@gmail.com>
2017-11-14 13:12:10 -0500

    create dockerfile

---
+++ create_dockerfile.sh
@@ -0,0 +1,20 @@
+# Generate Dockerfile.
+docker run --rm kaczmarj/neurodocker:master generate -b neurodebian:stretch-non-free -p apt \
+--fsl version=5.0.9 \
+--add-to-entrypoint "source /etc/fsl/fsl.sh" \
+--user neuro \
+--user root \
+--miniconda env_name=neuro \
+	conda_opts="--channel vida-nyu" \
+	conda_install="python=3.5.1 numpy pandas reprozip traits" \
+	pip_install="nipype" \
+	activate=true \
+--neurodebian os_codename="stretch" \
+              download_server="usa-nh" \
+              pkgs="dcm2niix git-annex-standalone datalad" \
+--user neuro \
+--workdir /home/neuro \
+--no-check-urls > Dockerfile
+
+# Build Docker image using the saved Dockerfile.
+docker build -t myimage -f Dockerfile .
